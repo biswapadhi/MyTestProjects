566cba4787e2c077fd3af14b0a24f4c9ecedbab4
Biswaranjan Padhi <74845006+biswapadhi@users.noreply.github.com>
2023-08-08 15:21:37 +0530

    First Commit

---
+++ UserManagementProject/src/com/padhi/dao/UserDao.java
@@ -0,0 +1,147 @@
+package com.padhi.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.padhi.bean.User;
+
+public class UserDao {
+	private String jdbcURL="jdbc:mysql://localhost:3306/usermanagement";
+	private String jdbcUserName="root";
+	private String jdbcPassword="root";
+	private String jdbcDriver="com.mysql.cj.jdbc.Driver";
+	
+	private static final String INSERT_USER_SQL="INSERT INTO users"+"(name,email,country) values"
+													+"(?,?,?);";
+	private static final String SELECT_USER_BY_ID="select id,name,email,country from users where id=?;";
+	
+	private static final String SELECT_ALL_USER="select * from users";
+	
+	private static final String DELETE_USER="delete from users where id=?;";
+	
+	private static final String UPDATE_USER="update users set name=?,email=?,country=? where id=?;";
+	
+	public UserDao() {
+		 
+	}
+	
+	protected Connection getConnection() {
+		Connection connection=null;
+		try {
+			Class.forName(jdbcDriver);
+			connection=DriverManager.getConnection(jdbcURL,jdbcUserName,jdbcPassword);
+		}
+		
+		catch(SQLException e){
+			e.printStackTrace();
+			
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		 return connection;
+	}
+	
+	//Insert user
+	public void insertUser(User user) throws SQLException{
+		try {
+			Connection connection=getConnection();
+			PreparedStatement preparedStatement=connection.prepareStatement(INSERT_USER_SQL);
+			preparedStatement.setString(1, user.getName());
+			preparedStatement.setString(2, user.getEmail());
+			preparedStatement.setString(3, user.getCountry());
+			preparedStatement.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			
+		}
+	}
+	 
+	
+	//Select user by id
+	
+	public User selectUser(int id) {
+		User user=null;
+		try {
+			Connection connection=getConnection();
+			PreparedStatement preparedStatement=connection.prepareStatement(SELECT_USER_BY_ID);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			
+			while(resultSet.next()) {
+				String name=resultSet.getString("name");
+				String email=resultSet.getString("email");
+				String country=resultSet.getString("country");
+				
+				user=new User(id,name,email,country);
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return user;
+		
+	}
+	
+	//select all user
+	public List<User> selectAllUser() {
+		List<User> users=new ArrayList<User>();
+		try {
+			Connection connection=getConnection();
+			PreparedStatement preparedStatement=connection.prepareStatement(SELECT_ALL_USER);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()) {
+				int id=resultSet.getInt("id");
+				String name=resultSet.getString("name");
+				String email=resultSet.getString("email");
+				String country=resultSet.getString("country");
+				
+				users.add(new User(id,name,email,country));
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return users; 
+	}
+	
+	//update user
+	 public boolean updateUser(User user) throws SQLException{
+		 boolean rowUpdated=false;
+		 try {
+			 Connection connection=getConnection();
+			 PreparedStatement preparedStatement=connection.prepareStatement(UPDATE_USER);
+			 preparedStatement.setString(1, user.getName());
+			 preparedStatement.setString(2, user.getEmail());
+			 preparedStatement.setString(3, user.getCountry());
+			 preparedStatement.setInt(4, user.getId());
+			 rowUpdated=preparedStatement.executeUpdate() > 0;// returns true or false
+		 }
+		 catch(SQLException e) {
+			 e.printStackTrace();
+		 }
+		 return rowUpdated;
+	 }
+	
+	//delete User
+	 public boolean deleteUser(int id) throws SQLException{
+		boolean rowDeleted=false  ;
+		try {
+			Connection connection=getConnection();
+			PreparedStatement preparedStatement=connection.prepareStatement(DELETE_USER);
+			preparedStatement.setInt(1, id);
+			rowDeleted=preparedStatement.executeUpdate() > 0;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return rowDeleted;
+	 }
+
+}
---
+++ UserManagementProject/src/com/padhi/web/UserServlet.java
@@ -0,0 +1,153 @@
+package com.padhi.web;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.padhi.bean.User;
+import com.padhi.dao.UserDao;
+
+@WebServlet("/")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	UserDao userDao;
+	
+	public void init() throws ServletException{
+	 userDao=new UserDao();
+	}
+   
+   
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String action=request.getServletPath();
+		switch(action) {
+		case "/new":
+			showNewForm(request,response);
+			break;
+		
+		case "/insert":
+			insertUser(request, response);
+			break;
+		
+		case "/delete":
+			deleteUser(request, response);
+			break;
+		
+		case "/edit":
+			try {
+				showEditForm(request, response);
+				} catch (SQLException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			} 
+			break;
+		
+		case "/update":
+			try {
+				updateUser(request, response);
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			break;
+		
+		default:
+			try {
+				listUser(request, response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		}
+	}
+		private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			RequestDispatcher dispatcher=request.getRequestDispatcher("user-form.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+		private void insertUser(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+			String name=request.getParameter("name");
+			String email=request.getParameter("email");
+			String country=request.getParameter("country");
+			
+			User newUser=new User(name,email,country);
+			  
+			try {
+				userDao.insertUser(newUser);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace(); 
+			}
+			response.sendRedirect("list");
+		}
+		
+		private void deleteUser(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+			int id=Integer.parseInt(request.getParameter("id"));
+			try {
+				userDao.deleteUser(id);
+				
+			}
+			catch(Exception e ) {
+				e.printStackTrace();
+			}
+			response.sendRedirect("list");
+		}
+		
+		private void showEditForm(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException,SQLException{
+			int id=Integer.parseInt(request.getParameter("id"));
+			User existingUser;
+			
+			try {
+				existingUser=userDao.selectUser(id);
+				RequestDispatcher requestDispatcher=request.getRequestDispatcher("user-form.jsp");
+				request.setAttribute("user", existingUser);
+				requestDispatcher.forward(request, response);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				
+			}
+		}
+		
+		private void updateUser(HttpServletRequest request,HttpServletResponse response) throws SQLException,IOException{
+			int id=Integer.parseInt(request.getParameter("id"));
+			String name=request.getParameter("name");
+			String email=request.getParameter("email");
+			String country=request.getParameter("country");
+			User user=new User(id,name,email,country);
+			userDao.updateUser(user);
+			response.sendRedirect("List");
+		}
+		
+		//default
+		
+		private void listUser(HttpServletRequest request,HttpServletResponse response) throws SQLException,IOException{
+			try {
+				List<User> listUser=userDao.selectAllUser();
+				request.setAttribute("listUser", listUser);
+				RequestDispatcher requestDispatcher=request.getRequestDispatcher("user-list.jsp");
+				requestDispatcher.forward(request, response);
+				
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		 
+		
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
